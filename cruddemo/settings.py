105e1d739606f367e7617337f5120252e44736be
Ahmad Ubaid <79589179+ahmadubaid47@users.noreply.github.com>
2021-08-01 00:24:30 +0500

    settings.py

--- cruddemo/settings.py
+++ cruddemo/settings.py
@@ -2,17 +2,16 @@
 
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+
+
 SECRET_KEY = 'kg$v(jq(1@ldbx^o^-k$-b3$35z+yvtf9p+js^g#!=mb$*eqo2'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
@@ -62,7 +61,6 @@ WSGI_APPLICATION = 'cruddemo.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -73,7 +71,7 @@ DATABASES = {
 
 
 # Password validation
-# https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
